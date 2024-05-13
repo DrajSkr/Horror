9030e400c52e4129b2501b25743f2645ce2ab21e
DrajSkr <146635680+DrajSkr@users.noreply.github.com>
2024-05-13 18:21:37 +0530

    change lol

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,11 @@
-extends Node3D
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("hiiii")
-	
+	pass

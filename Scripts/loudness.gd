7bada300c68b5ae6d0c5dbd2656a9078f3e19014
DrajSkr <146635680+DrajSkr@users.noreply.github.com>
2024-05-17 14:31:37 +0530

    voice detection

---
+++ Scripts/loudness.gd
@@ -0,0 +1,36 @@
+extends Node
+
+@onready var item_list = get_node("ItemList")
+@onready var Awazmeter = $Awazmeter
+@onready var device_info = $DeviceInfo
+
+
+func _ready():
+	for item in AudioServer.get_input_device_list():
+		item_list.add_item(item)
+
+	var device = AudioServer.input_device
+	for i in range(item_list.get_item_count()):
+		if device == item_list.get_item_text(i):
+			item_list.select(i)
+			break
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var power = AudioServer.get_bus_peak_volume_left_db(AudioServer.get_bus_index("Record"),0) + 200
+	if(power>100):
+		Awazmeter.value = power
+	else:
+		Awazmeter.value=0
+	device_info.text = "Currently selected: " + AudioServer.input_device
+	
+	
+
+func _on_select_pressed():
+	for item in item_list.get_selected_items():
+		var device = item_list.get_item_text(item)
+		AudioServer.input_device = device
+
+
+func _on_reload_pressed():
+	get_tree().reload_current_scene()
